74cbb097de12fef5429d3d854b29c8c36b6861a0
Spurkus <oscartangplayz@gmail.com>
2023-08-25 20:30:15 +1000

    Actually the base template I think v2

--- src/app/components/buildingDle.tsx
+++ src/app/components/buildingDle.tsx
@@ -2,6 +2,7 @@
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import ListGroup from 'react-bootstrap/ListGroup';
 
 function BuildingDle() {
   // Please change search form to dropdown thing please ty thanks :)
@@ -16,6 +17,12 @@ function BuildingDle() {
             />
             <Button variant="success">Guess!</Button>
         </Form>
+        <ListGroup key="md" horizontal="md" className="my-2">
+          <ListGroup.Item>Advanced Engineering Building</ListGroup.Item>
+          <ListGroup.Item>{'>'} 49</ListGroup.Item>
+          <ListGroup.Item>Staff House Road Precinct</ListGroup.Item>
+          <ListGroup.Item>100m</ListGroup.Item>
+        </ListGroup>
     </Container>
   );
 }
