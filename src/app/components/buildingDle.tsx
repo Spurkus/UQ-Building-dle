4d717bc00038ce7f34dc939250c1a6ad6b7cf1d1
Spurkus <oscartangplayz@gmail.com>
2023-08-25 22:12:16 +1000

    Logo and started buildingdleing ❤️

--- src/app/components/buildingDle.tsx
+++ src/app/components/buildingDle.tsx
@@ -1,19 +1,28 @@
 'use client'
+import { useState } from 'react'
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
+import Select from 'react-select';
 import Button from 'react-bootstrap/Button';
 import ListGroup from 'react-bootstrap/ListGroup';
 
+  const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' }
+  ] // Temporary Data
+
 function BuildingDle() {
   // Please change search form to dropdown thing please ty thanks :)
+  const [buildingName, setBuildingName] = useState();
   return (
     <Container className="justify-content-center">
         <Form className="d-flex">
-            <Form.Control
-              type="search"
+            <Select
               placeholder="Search"
-              className="me-2"
+              className="me-2 flex-fill"
               aria-label="Search"
+              options={options}
             />
             <Button variant="success">Guess!</Button>
         </Form>
