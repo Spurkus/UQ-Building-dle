60b76333eca6aa9cefae13f56e5b9c9e48dcae94
roxiesusername <143169389+roxiesusername@users.noreply.github.com>
2023-08-26 17:48:13 +1000

    I don't remember what I did

--- src/app/components/guess.tsx
+++ src/app/components/guess.tsx
@@ -22,8 +22,8 @@ const Guess: React.FC<GuessProps> = ({buildings, num, correct_num}) => {
     const {precinct: pAnswer, latitude: la2, longitude: ll2 } = buildings[correct_num]; 
     return <ListGroup key="md" horizontal="md" className= "my-2 flex-fill d-flex">
         <ListGroup.Item className="flex-fill">{name}</ListGroup.Item>
-        <ListGroup.Item className="flex-fill">{parseInt(correct_num) > parseInt(num) ? '>' : '<'}{num}</ListGroup.Item>
-        <ListGroup.Item className="flex-fill" style={pAnswer === pGuess? {backgroundColor: "#6fe388"} : {backgroundColor: "#e65e5e"}}>{pGuess}</ListGroup.Item>
+        <ListGroup.Item className="flex-fill">{num}</ListGroup.Item>
+        <ListGroup.Item className="flex-fill" style={pAnswer === pGuess? {backgroundColor: "#6fe388"} : {backgroundColor: "white"}}>{pGuess}</ListGroup.Item>
         {/* TODO: remove the ! */}
         <ListGroup.Item className="flex-fill">{coordDistance(la1!, ll1!, la2!, ll2!)}m</ListGroup.Item>
     </ListGroup>
