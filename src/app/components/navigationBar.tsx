eef9d4d06dd040bfd12e0bdecf43d64cc9fda6ee
Spurkus <oscartangplayz@gmail.com>
2023-08-26 17:24:26 +1000

    settings template

--- src/app/components/navigationBar.tsx
+++ src/app/components/navigationBar.tsx
@@ -1,11 +1,43 @@
 'use client'
+import { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import ToggleButton from 'react-bootstrap/ToggleButton';
+import Modal from 'react-bootstrap/Modal';
 import Image from 'next/image';
 
 function NavigationBar() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+
+  const [checked, setChecked] = useState(false);
+  const [difficultyValue, setDifficultyValue] = useState('1');
+
+  const difficulties = [
+    { name: 'Easy', value: '1' },
+    { name: 'Medium', value: '2' },
+    { name: 'Hard', value: '3' },
+    { name: 'Impossible', value: '4'}
+  ];
+
+  function variant(id: number) {
+    if (id == 1) {
+      return "outline-success"
+    } else if (id == 2) {
+      return "outline-info"
+    } else if (id == 3) {
+      return "outline-warning"
+    } else {
+      return "outline-danger"
+    }
+  }
+
   return (
     <Navbar bg="dark" data-bs-theme="dark" className="bg-body-tertiary" expand="lg">
       <Container className="justify-content-center">
@@ -23,9 +55,41 @@ function NavigationBar() {
           <Nav className="me-auto">
             <Nav.Link href="https://github.com/Spurkus/UQ-Building-dle">Github Repo</Nav.Link>
           </Nav>
-          <Button variant="outline-info">Settings ⚙️</Button>
+          <Button variant="outline-info" onClick={handleShow}>Settings ⚙️</Button>
         </Navbar.Collapse>
       </Container>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Settings</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>Click on buttons to change difficulty.</p>
+          <ButtonGroup>
+            {difficulties.map((difficulty, idx) => (
+              <ToggleButton
+                key={idx}
+                id={`difficulty-${idx}`}
+                type="radio"
+                variant={variant(idx + 1)}
+                name="difficulty"
+                value={difficulty.value}
+                checked={difficultyValue === difficulty.value}
+                onChange={(e) => setDifficultyValue(e.currentTarget.value)}
+              >
+                {difficulty.name}
+              </ToggleButton>
+            ))}
+        </ButtonGroup>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </Navbar>
   );
 }
