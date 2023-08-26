0464b361eb07e9e1134063a2049d6e663781603e
Spurkus <oscartangplayz@gmail.com>
2023-08-26 11:47:15 +1000

    added map kinda!!

---
+++ src/app/components/map.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+function Map() {
+  return (
+    <div className="text-center d-flex flex-column" style={{height: "calc(100vh - 55px)"}}>
+        <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3539.09251811908!2d153.01111021038042!3d-27.49749801779115!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6b91508241eb7c49%3A0x9ae9946d3710eee9!2sThe%20University%20of%20Queensland!5e0!3m2!1sen!2sau!4v1693012952105!5m2!1sen!2sau"
+            width="100%"
+            height="100%"
+            style={{border: 0}}
+            loading="lazy"
+            />
+    </div>
+  );
+}
+
+export default Map;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,24 @@
 'use client'
 import "bootstrap/dist/css/bootstrap.min.css"; // Import bootstrap CSS
-import NavigationBar from './components/navigationBar';
+import NavigationBar from "./components/navigationBar";
 import BuildingDle from "./components/buildingDle";
+import Map from "./components/map";
 import { Container, Row, Col } from "react-bootstrap";
 
 export default function Home() {
   return (
     <main>
       <NavigationBar />
-      <Container>
       <Row>
-        <Col>Map implement Maybe</Col>
-        <Col xs={6}>
-          <div className="text-center bg-secondary vh-100 d-flex flex-column">
+        <Col xs={4} style={{margin: 0, padding: 0, border: 0}}><Map /></Col>
+        <Col xs={6} style={{margin: 0, padding: 0, border: 0}}>
+          <div className="text-center bg-secondary d-flex flex-column" style={{height: '100%'}}>
             <h4>Main Section</h4>
             <BuildingDle />
           </div>
         </Col>
         <Col><p>Win/Lose Rate <br></br> Leader board</p></Col>
       </Row>
-    </Container>
     </main>
   )
 }
