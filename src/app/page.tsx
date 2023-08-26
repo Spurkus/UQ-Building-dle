bc61bd86ae9865944f1ac8b01e74030974df64a2
goanna5 <143087684+goanna5@users.noreply.github.com>
2023-08-26 12:00:11 +1000

    added text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,10 @@ export default function Home() {
         <Col xs={4} style={{margin: 0, padding: 0, border: 0}}><Map /></Col>
         <Col xs={6} style={{margin: 0, padding: 0, border: 0}}>
           <div className="text-center bg-secondary d-flex flex-column" style={{height: '100%'}}>
-            <h4>Main Section</h4>
+            <h4>Welcome to Building-dle</h4>
+            <p>A random UQ building has been chosen</p>
+            <p>Guess what it is and we'll give you hints if you're wrong</p>
+
             <BuildingDle />
           </div>
         </Col>
