d4c07d97e734c7e0f2c3fbb524184c00224545c8
goanna5 <143087684+goanna5@users.noreply.github.com>
2023-08-26 12:15:06 +1000

    its now white text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       <Row>
         <Col xs={4} style={{margin: 0, padding: 0, border: 0}}><Map /></Col>
         <Col xs={6} style={{margin: 0, padding: 0, border: 0}}>
-          <div className="text-center bg-secondary d-flex flex-column" style={{height: '100%'}}>
+          <div className="text-center bg-secondary d-flex flex-column" style={{height: '100%', color: "white"}}>
             <h4>Welcome to UQ-Building-dle</h4>
             <p>A random UQ building has been chosen</p>
             <p>Guess what it is and we'll give you hints if you're wrong</p>
