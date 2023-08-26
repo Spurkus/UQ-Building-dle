93501a51c6a6296830217598bb732eb969504674
goanna5 <143087684+goanna5@users.noreply.github.com>
2023-08-26 12:17:15 +1000

    menu changed to better

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,10 +12,12 @@ export default function Home() {
       <Row>
         <Col xs={4} style={{margin: 0, padding: 0, border: 0}}><Map /></Col>
         <Col xs={6} style={{margin: 0, padding: 0, border: 0}}>
-          <div className="text-center bg-secondary d-flex flex-column" style={{height: '100%', color: "white"}}>
+          <div className="text-center bg-secondary d-flex flex-column" style={{height: '100%'}}>
+            <div style={{color: "white"}}>
             <h4>Welcome to UQ-Building-dle</h4>
             <p>A random UQ building has been chosen</p>
             <p>Guess what it is and we'll give you hints if you're wrong</p>
+            </div>
 
             <BuildingDle />
           </div>
