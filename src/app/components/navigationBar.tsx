daddbc5bb42ac8cdceec8b7812fa7861283257f2
kowasaur <75297283+kowasaur@users.noreply.github.com>
2023-08-27 12:04:53 +1000

    make it compile (hopefully)

--- src/app/components/navigationBar.tsx
+++ src/app/components/navigationBar.tsx
@@ -105,10 +105,10 @@ function NavigationBar() {
           <Modal.Title style={{textAlign: "center", width: "100%"}}>Instructions</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <p>Welcome to UQ-Building-dle! Here's how to play:</p>
-          <p>Goal: Find the mystery building utilising two maps: a map of UQ and a precinct map. Please note that this precinct map only displays the precinct you've chosen, which might not necessarily be the correct one.</p>
+          <p>Welcome to UQ-Building-dle! Here{"'"}s how to play:</p>
+          <p>Goal: Find the mystery building utilising two maps: a map of UQ and a precinct map. Please note that this precinct map only displays the precinct you{"'"}ve chosen, which might not necessarily be the correct one.</p>
           <p>Step One: Begin by choosing a building using the search bar or drop-down menu.</p>
-          <p>Step Two: Watch the building numbers. They'll turn greener as you get closer to the right building number.</p>
+          <p>Step Two: Watch the building numbers. They{"'"}ll turn greener as you get closer to the right building number.</p>
           <p>Step Three: If you find the right precinct, it will turn green.</p>
           <p>Step Four: Check the distance indicator. It shows how far your choice is from the real mystery building.</p>
         <Button onClick={() => setShowInstructionsModal(false)}>Close</Button>
